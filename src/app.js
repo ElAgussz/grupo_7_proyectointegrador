eaf2dd8a2dee5b013e1b15c7dc65455d70266266
Agustin Surroca <aguss_n0@hotmail.com>
2022-03-04 17:40:16 -0300

    Se configuró el app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+const express = require("express");
+const path = require("path");
+const port = 3000;
+const app = express();
+const publicPath = path.join(__dirname, "../public")
 
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log("Se inició el servidor en el puerto " + port)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"))
+})
+
+app.get('/producto', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "productDetail.html"))
+})
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "productCart.html"))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "login.html"))
+})
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "register.html"))
+})
