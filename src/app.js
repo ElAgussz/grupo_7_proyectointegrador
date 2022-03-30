898a34dfe2eeb6fe4dd9798f36f806104f94f3ac
ElAgussz <aguss_n0@hotmail.com>
2022-03-30 11:48:02 -0300

    instalacion del _method + ordenar un poco el app.js

--- src/app.js
+++ src/app.js
@@ -3,19 +3,24 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const publicPath = path.join(__dirname, "../", "public")
-const viewspath = path.join(__dirname, "../src/views")
-app.set("views", viewspath);
 const router = require('./routes/rutas.js');
 const routerProducts = require('./routes/routesProducts.js');
+const methodOverride = require("method-override");
+const viewspath = path.join(__dirname, "../src/views")
+
+
+app.set("views", viewspath);
 app.set ("views engine", "ejs")
+
+
 app.use(express.static(publicPath));
+app.use(methodOverride("_method"))
+
+
+app.use('/', router);
+app.use('/', routerProducts)
 
 
 app.listen(PORT, () => {
     console.log ("Se inició el servidor en el puerto " + PORT)
 })
-
-app.use('/', router);
-
-
-app.use('/', routerProducts)
