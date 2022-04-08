6ffb8e3957a154a6e62bfa08afd0e65c484683a4
ramirezma <ramirezma90@gmail.com>
2022-04-07 21:55:22 -0300

    path a json

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,4 +1,11 @@
-const path = require ("path")
+const path = require ("path");
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 const controlador = {
     index: (req, res) => {
         res.render ("index.ejs")
--- src/controller/controllerProducts.js
+++ src/controller/controllerProducts.js
@@ -1,4 +1,11 @@
 const path = require ("path")
+const fs = require('fs');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 const controlador = {
     carrito: (req, res) => {
         res.render("productCart.ejs");
