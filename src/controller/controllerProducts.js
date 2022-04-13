8fe98794ff3b9e1a80b76b84c1b28c5e206c5c1c
ElAgussz <aguss_n0@hotmail.com>
2022-04-13 14:58:40 -0300

    se modifico el diseño del login

--- src/controller/controllerProducts.js
+++ src/controller/controllerProducts.js
@@ -1,4 +1,4 @@
-const path = require ("path")
+const path = require("path")
 const fs = require('fs');
 
 const productsFilePath = path.join(__dirname, '../data/products.json');
@@ -13,22 +13,26 @@ const controlador = {
     producto: (req, res) => {
         res.render("productDetail.ejs");
     },
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
     editar: (req, res) => {
         const id = req.params.id;
         const product = products.find(product => product.id == id);
-        res.redirect("/products");
+        return res.redirect("/products", { product });
     },
     borrar: (req, res) => {
-       const id = req.params.id;
-       products = products.filter(product => product.id != id);
-       res.redirect("/products");
+        const id = req.params.id;
+        products = products.map(product => {
+            if (product.id != id) {
+                return product;
+            }
+        })
+        return res.redirect("/products");
     },
 
 }
