6b73815431c21b44a688af7e4bc1a63fcacec303
ElAgussz <aguss_n0@hotmail.com>
2022-05-14 12:37:06 -0300

    multer para el formulario de registro + carpeta avatars en images

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const app = express();
 app.set("views", viewspath);
 app.set ("view engine", "ejs")
 
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(publicPath));
 app.use(methodOverride("_method"))
 app.use(session({
--- src/routes/routesUser.js
+++ src/routes/routesUser.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const controlador = require('../controller/controllerUser');
-
+const multer = require('multer');
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require("../middlewares/authMiddleware")
 
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '../public/images/avatars')
+    },
+    filename: (req, file, cb) => {
+        let fileName = `${Date.now()}_img${path.extname(file.filename)}`;
+        cb(null, filename)
+    }
+})
+
+const upload = multer({storage});
+
+
 router.get('/register', guestMiddleware, controlador.register) // Vista del formulario de registro
 
-router.post('register', controlador.processRegister) // Procesar el registro
+router.post('register', upload.single("avatar"), controlador.processRegister) // Procesar el registro
 
 // router.get ('/register-confirmation', controlador.registerConfirmacion)
 
