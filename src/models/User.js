a0f650b8f930008d6c27594f0a304ee932f8fa1d
juan Rinero <juanrinero@gmail.com>
2022-05-16 20:12:20 -0300

    arreglo

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const User = {
-	fileName: usuariosPath,
+	fileName: "../data/users.json",
 
 	getData: function () {
 		return JSON.parse(fs.readFileSync(this.fileName, 'utf-8'));
